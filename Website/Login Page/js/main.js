664436cf65cc070db0df688d16162d61cb01b51d
Samir ElBatal <samirelbatal0@gmail.com>
2024-04-25 20:23:29 +0200

    Organization Register

--- Website/Login Page/js/main.js
+++ Website/Login Page/js/main.js
@@ -1,48 +1,46 @@
 const inputs = document.querySelectorAll(".input");
 
-
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
+function addcl() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
 }
 
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
+function remcl() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
 }
 
-
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+inputs.forEach((input) => {
+  input.addEventListener("focus", addcl);
+  input.addEventListener("blur", remcl);
 });
 
 function login(event) {
-    event.preventDefault(); // Prevent form submission
+  event.preventDefault(); // Prevent form submission
 
-    var username = document.querySelector(".input[type='text']").value;
-    var password = document.querySelector(".input[type='password']").value;
+  var username = document.querySelector(".input[type='text']").value;
+  var password = document.querySelector(".input[type='password']").value;
 
-    // Check if username and password match
-    if (username === 'omar' && password === 'gaber') {
-        // Redirect to another page
-        window.location.href =  '../HomePage/homepage.html'
-    } else {
-        // Display error message or handle invalid credentials
-		displayErrorMessage('Invalid username or password. Please try again.');
-    }
+  // Check if username and password match
+  if (username === "user" && password === "password") {
+    // Redirect to another page
+    window.location.href = "../HomePage/homepage.html";
+  } else {
+    // Display error message or handle invalid credentials
+    displayErrorMessage("Invalid username or password. Please try again.");
+  }
 }
 function displayErrorMessage(message) {
-    var errorMessageLabel = document.querySelector('.error-message-label');
-    errorMessageLabel.textContent = message;
-    errorMessageLabel.classList.add('error-message');
+  var errorMessageLabel = document.querySelector(".error-message-label");
+  errorMessageLabel.textContent = message;
+  errorMessageLabel.classList.add("error-message");
 
-    // Fade away after 3 seconds
-    setTimeout(function() {
-        errorMessageLabel.textContent = ''; // Clear the error message
-        errorMessageLabel.classList.remove('error-message');
-    }, 3000);
+  // Fade away after 3 seconds
+  setTimeout(function () {
+    errorMessageLabel.textContent = ""; // Clear the error message
+    errorMessageLabel.classList.remove("error-message");
+  }, 3000);
 }
 document.querySelector(".btn").addEventListener("click", login);
