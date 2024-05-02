5206c4924d1d032046f067464030ab9fe9a196a9
Samir ElBatal <samirelbatal0@gmail.com>
2024-05-02 17:24:51 +0300

    Dynamic Data flow for Organization Profile

---
+++ Website/Donor/homePage/oraganizations/organizationProfile.js
@@ -0,0 +1,35 @@
+// Function to get URL query parameters
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+  }
+  
+  // Retrieve query parameters
+  const id = getQueryParam("id");
+  const organization = getQueryParam("organization");
+  const address = getQueryParam("address");
+  const area = getQueryParam("area");
+  const governorate = getQueryParam("governorate");
+  const type = getQueryParam("type");
+  const email = getQueryParam("email");
+  const telephone = getQueryParam("telephone");
+  const googleMap = getQueryParam("googleMap");
+
+   
+    document.getElementById("label1-1").innerText = organization;
+    document.getElementById("label1-2").innerText = telephone + ", " + email;
+    document.getElementById("label1-3").innerText = address;
+    document.getElementById("label1-4").innerText = type;
+    document.getElementById("label1").innerText = "Name:";
+    document.getElementById("label2").innerText = "Contact Details:";
+    document.getElementById("label3").innerText = "Address:";
+    document.getElementById("label4").innerText = "Type:";
+  
+  
+  document.addEventListener("DOMContentLoaded", function() {
+      // Hide loader after 2 seconds
+      setTimeout(function() {
+        document.getElementById("loader").style.display = "none";
+      }, 1000); // 2000 milliseconds = 2 seconds
+    });
+    
