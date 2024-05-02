54cbe319d4c0921c22247bd61d7dcc85d240f10e
Samir ElBatal <samirelbatal0@gmail.com>
2024-05-02 20:28:44 +0300

    adding organization base files and fixing nav bar feature

--- Website/Donor/homePage/js/script.js
+++ Website/Donor/homePage/js/script.js
@@ -1,15 +1,3 @@
-document.querySelectorAll('.image-container img').forEach(image => {
-    image.onclick = () => {
-        document.querySelector('.pop-image').style.display = 'block';
-        document.querySelector('.pop-image img').src = image.getAttribute('src');
-    }
-});
-
-document.querySelector('.pop-image span').onclick = () => {
-    document.querySelector('.pop-image').style.display = 'none';
-}
-
-
 
 document.addEventListener("DOMContentLoaded", function() {
   var donationRequests = document.querySelector('a[href="#team"]');
@@ -44,10 +32,20 @@ nestedList.style.display = 'none';
 });
 });
 
-
 document.addEventListener("DOMContentLoaded", function() {
   // Hide loader after 2 seconds
   setTimeout(function() {
     document.getElementById("loader").style.display = "none";
   }, 1000); // 2000 milliseconds = 2 seconds
+});
+
+
+$(document).ready(function() {
+  $('.navbar-nav .nav-item:nth-child(1)').addClass('active');
+
+  $('.navbar-nav .nav-item .nav-link').click(function() {
+    $('.navbar-nav .nav-item').removeClass('active');
+
+    $(this).closest('.nav-item').addClass('active');
+  });
 });
--- Website/Organization/homePage/js/script.js
+++ Website/Organization/homePage/js/script.js
@@ -1,10 +1,51 @@
-document.querySelectorAll('.image-container img').forEach(image => {
-    image.onclick = () => {
-        document.querySelector('.pop-image').style.display = 'block';
-        document.querySelector('.pop-image img').src = image.getAttribute('src');
-    }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var donationRequests = document.querySelector('a[href="#team"]');
+  
+  var donationItems = document.querySelector('.dropdown-menu ul.dropdown-menu');
+
+  donationItems.style.display = 'none';
+
+  donationRequests.addEventListener("mouseover", function() {
+    donationItems.style.display = 'block';
+  });
+
+  donationRequests.addEventListener("mouseout", function() {
+    donationItems.style.display = 'none';
+  });
+});
+document.addEventListener("DOMContentLoaded", function() {
+var donationItems = document.querySelectorAll('.dropdown-menu ul.dropdown-menu li');
+
+var nestedList = document.querySelector('.dropdown-menu ul.dropdown-menu');
+
+nestedList.style.display = 'none';
+
+donationItems.forEach(function(item) {
+item.addEventListener("mouseover", function() {
+nestedList.style.display = 'block';
+});
 });
 
-document.querySelector('.pop-image span').onclick = () => {
-    document.querySelector('.pop-image').style.display = 'none';
-}
+nestedList.addEventListener("mouseout", function() {
+nestedList.style.display = 'none';
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Hide loader after 2 seconds
+  setTimeout(function() {
+    document.getElementById("loader").style.display = "none";
+  }, 1000); // 2000 milliseconds = 2 seconds
+});
+
+
+$(document).ready(function() {
+  $('.navbar-nav .nav-item:nth-child(1)').addClass('active');
+
+  $('.navbar-nav .nav-item .nav-link').click(function() {
+    $('.navbar-nav .nav-item').removeClass('active');
+
+    $(this).closest('.nav-item').addClass('active');
+  });
+});
