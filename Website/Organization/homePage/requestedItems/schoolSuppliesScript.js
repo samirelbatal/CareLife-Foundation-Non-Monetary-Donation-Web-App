7419577730f530a1c6821f9c34eabbaac9aee050
Samir ElBatal <samirelbatal0@gmail.com>
2024-05-04 18:17:02 +0300

    charity organization notification center

--- Website/Organization/homePage/requestedItems/schoolSuppliesScript.js
+++ Website/Organization/homePage/requestedItems/schoolSuppliesScript.js
@@ -263,3 +263,24 @@ $(document).ready(function() {
     $(this).closest('.nav-item').addClass('active');
   });
 });
+
+var subMenu1 = document.getElementById('subMenu1');
+var subMenu2 = document.getElementById('subMenu2');
+
+function toggleMenu() {
+  // Check if subMenu2 is open, if yes, close it
+  if (subMenu2.classList.contains("open-menu")) {
+    subMenu2.classList.remove("open-menu");
+  }
+  // Toggle open-menu class for subMenu1
+  subMenu1.classList.toggle("open-menu");
+}
+
+function toggleBell() {
+  // Check if subMenu2 is open, if yes, close it
+  if (subMenu1.classList.contains("open-menu")) {
+    subMenu1.classList.remove("open-menu");
+  }
+  // Toggle open-menu class for subMenu1
+  subMenu2.classList.toggle("open-menu");
+}
