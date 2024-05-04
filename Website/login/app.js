cb929372bab7ec7216f65bba7027c04c7c34dda9
Omar Gaber <omargaber1103@gmail.com>
2024-05-04 18:46:04 +0300

    updated functionality and css of login

--- Website/login/app.js
+++ Website/login/app.js
@@ -1,18 +1,41 @@
-const sign_in_btn = document.querySelector("#sign-in-btn");
-const login_btn = document.querySelector("#login-btn");
-const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
 
-sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
-});
-login_btn.addEventListener("click", () => {
-    window.location.href = "test.html";
-  });
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const donor_sign_up_btn = document.getElementById("sign-up-btn");
+const charity_sign_up_btn = document.getElementById("charity-sign-up-btn");
+const container = document.querySelector(".container");
+const donor_sign_up_form = document.querySelector("#donor_sign_up_form");
+const charity_sign_up_form = document.querySelector("#charity_sign_up_form");
+const sign_in_form = document.querySelector(".sign-in-form");
 
 sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+    container.classList.remove("sign-up-mode");
+    donor_sign_up_form.style.display = "none";
+    charity_sign_up_form.style.display = "none";
 });
+
+donor_sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+    charity_sign_up_form.style.display = "none";
+   // sign_in_form.style.display = "none";
+   setTimeout(() => {
+    donor_sign_up_form.style.display = "block";
+}, 700); // Show the donor sign-up form
+    
+});
+
+charity_sign_up_btn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+    //sign_in_form.style.display = "none";
+    donor_sign_up_form.style.display = "none";
+    setTimeout(() => {
+        charity_sign_up_form.style.display = "block";
+    }, 700) // Show the charity sign-up form
+});
+
+
+
+
 function authenticateAndRedirect() {
     // Here you would have your authentication logic
     var username = document.getElementById("username").value; // Assuming you have input fields with IDs "username" and "password"
