0f47525d4a53e6ce23841e0552496103da77e238
Samir ElBatal <samirelbatal0@gmail.com>
2024-05-05 02:38:28 +0300

    Organization Create Post

---
+++ Website/Organization/homePage/Profile/viewProfile.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Hide loader after 2 seconds
+  setTimeout(function () {
+    document.getElementById("loader").style.display = "none";
+  }, 1000); // 2000 milliseconds = 2 seconds
+});
+
+var subMenu1 = document.getElementById("subMenu1");
+var subMenu2 = document.getElementById("subMenu2");
+
+function toggleMenu() {
+  // Check if subMenu2 is open, if yes, close it
+  if (subMenu2.classList.contains("open-menu")) {
+    subMenu2.classList.remove("open-menu");
+  }
+  // Toggle open-menu class for subMenu1
+  subMenu1.classList.toggle("open-menu");
+}
+
+function toggleBell() {
+  // Check if subMenu2 is open, if yes, close it
+  if (subMenu1.classList.contains("open-menu")) {
+    subMenu1.classList.remove("open-menu");
+  }
+  // Toggle open-menu class for subMenu1
+  subMenu2.classList.toggle("open-menu");
+}
